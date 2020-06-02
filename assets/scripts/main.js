63f115f0867cd8313e895edfaa9db3375bc88773
Jimmy_dingo <you@example.com>
2020-06-02 12:25:22 +0300

    animation menu with promises added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,47 +4,58 @@
 
 //************************************ Menu Functions ********************************* */
 
-//*** Mobile menu trigger ***//
+//******** Mobile menu trigger ********//
 const hamburgerBtn = document.querySelector('.hamburger-menu-icon img');
 const mobileMenu = document.querySelector('.main-nav-mobile');
 const closeMenuBtn = document.querySelector('.close-menu-icon');
 
-const animationMenu = (widthPercentage, delayMs) => {
-    setTimeout(() => {
-        mobileMenu.style.width = widthPercentage;
-    }, delayMs);
-};
 
-//*** Open button ***//
+//******** Open button ********//
 
-hamburgerBtn.addEventListener('click', ()=>{
+const applyWidth = (width) => mobileMenu.style.width = `${width}%`;
+
+const menuAnimationOpen = () =>{
     mobileMenu.style.display = 'flex';
     hamburgerBtn.src= 'assets/icons/hamburger-icon-menu-opened.png';
-    setTimeout(()=>{
-        mobileMenu.style.width = '25%';
-        setTimeout(()=>{
-            mobileMenu.style.width = '50%';
-            setTimeout(()=>{
-                mobileMenu.style.width = '75%';
-            },30);
-        },30);
-    },30);
-});
+    const widthAndWait = (width) =>{
+        applyWidth(width)
+        return wait(30) //* delay of the animation *//
+    }
+    wait(30)
+    .then(widthAndWait(15)) //* Increments the width of the menu *//
+    .then(widthAndWait(30))
+    .then(widthAndWait(45))
+    .then(widthAndWait(60))
+    .then(widthAndWait(75))
+}
 
-//*** Close button ***//
+//* Promise declaration*//
+const wait = (timeMs) => {
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, timeMs);
+    })
+}
 
-closeMenuBtn.addEventListener('click', ()=>{
-    setTimeout(()=>{
-        mobileMenu.style.width = '75%';
-        setTimeout(()=>{
-            mobileMenu.style.width = '50%';
-            setTimeout(()=>{
-                mobileMenu.style.width = '25%';
-                setTimeout(()=>{
-                    mobileMenu.style.display = 'none';
-                    hamburgerBtn.src= 'assets/icons/hamburger-icon-menu-closed.png';
-                },30);
-            },30);
-        },30);
-    },30);
-});
+//* Event handler *//
+hamburgerBtn.addEventListener('click', menuAnimationOpen);
+
+
+//******** Close button ********//
+
+const menuAnimationClose = () =>{
+    hamburgerBtn.src= 'assets/icons/hamburger-icon-menu-closed.png';
+    const widthAndWait = (width) =>{
+        applyWidth(width)
+        return wait(30) //* delay of the animation *//
+    }
+    wait(30)
+    .then(widthAndWait(75)) //* Decrements the width of the menu *//
+    .then(widthAndWait(60))
+    .then(widthAndWait(45))
+    .then(widthAndWait(30))
+    .then(widthAndWait(15))
+    .then(()=> mobileMenu.style.display = 'none')
+}
+
+//* Event handler *//
+closeMenuBtn.addEventListener('click', menuAnimationClose);
