370c50614c721de2f6608535658e8db509347049
Jimmy_dingo <you@example.com>
2020-06-16 15:33:43 +0300

    Applied requestAnimationFrame to mobileMenu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,58 +4,103 @@
 
 //************************************ Menu Functions ********************************* */
 
-//******** Mobile menu trigger ********//
+//-------- Mobile Menu trigger --------//
+
 const hamburgerBtn = document.querySelector('.hamburger-menu-icon img');
 const mobileMenu = document.querySelector('.main-nav-mobile');
 const closeMenuBtn = document.querySelector('.close-menu-icon');
+const menuList = document.querySelectorAll('.main-nav-mobile li a');
+const iconSocial = document.querySelectorAll('.icon-social');
 
-
-//******** Open button ********//
+//-------------------------------------//
+//------------- Open Menu -------------//
 
 const applyWidth = (width) => mobileMenu.style.width = `${width}%`;
 
-const menuAnimationOpen = () =>{
-    mobileMenu.style.display = 'flex';
-    hamburgerBtn.src= 'assets/icons/hamburger-icon-menu-opened.png';
-    const widthAndWait = (width) =>{
-        applyWidth(width)
-        return wait(30) //* delay of the animation *//
-    }
-    wait(30)
-    .then(widthAndWait(15)) //* Increments the width of the menu *//
-    .then(widthAndWait(30))
-    .then(widthAndWait(45))
-    .then(widthAndWait(60))
-    .then(widthAndWait(75))
-}
+let currentMenuWidth = 0;
 
-//* Promise declaration*//
-const wait = (timeMs) => {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, timeMs);
-    })
+const menuAnimationOpen = () => {
+    mobileMenu.style.display = 'flex';
+    hamburgerBtn.src = 'assets/icons/hamburger-icon-menu-opened.png';
+    currentMenuWidth += 2;
+
+    applyWidth(currentMenuWidth +1);
+
+    if (Math.abs(currentMenuWidth) >= 74) {
+
+        //Menu elements iteration
+        for (let elem of menuList) {
+
+            if (Math.abs(currentMenuWidth) >= 55) {
+                //It shows the elements after the width of the menu is over 55%
+                elem.style.display = 'block';
+                closeMenuBtn.style.display = 'block';
+            };
+
+        }
+
+        //Icons iteration
+        for (let elem of iconSocial) {
+
+            if (Math.abs(currentMenuWidth) >= 55) {
+                //It shows the elements after the width of the menu is over 55%
+                elem.style.display = 'block';
+            };
+
+        }
+
+
+        return false
+    };
+
+    requestAnimationFrame(menuAnimationOpen);
 }
 
 //* Event handler *//
 hamburgerBtn.addEventListener('click', menuAnimationOpen);
 
 
-//******** Close button ********//
+//-------------------------------------//
+//------------- Close Menu ------------//
 
-const menuAnimationClose = () =>{
-    hamburgerBtn.src= 'assets/icons/hamburger-icon-menu-closed.png';
-    const widthAndWait = (width) =>{
-        applyWidth(width)
-        return wait(30) //* delay of the animation *//
+
+const menuAnimationClose = () => {
+    hamburgerBtn.src = 'assets/icons/hamburger-icon-menu-closed.png';
+    currentMenuWidth -= 2;
+    applyWidth(currentMenuWidth);
+
+    //Menu elements iteration
+    for (let elem of menuList) {
+        if (Math.abs(currentMenuWidth) <= 55) {
+            //It removes the elements after the width of the menu is under 55%
+            elem.style.display = 'none';
+            closeMenuBtn.style.display = 'none';
+        };
     }
-    wait(30)
-    .then(widthAndWait(75)) //* Decrements the width of the menu *//
-    .then(widthAndWait(60))
-    .then(widthAndWait(45))
-    .then(widthAndWait(30))
-    .then(widthAndWait(15))
-    .then(()=> mobileMenu.style.display = 'none')
+
+
+    //Icons iteration
+    for (let elem of iconSocial) {
+
+        if (Math.abs(currentMenuWidth) >= 55) {
+            //It shows the elements after the width of the menu is under 55%
+            elem.style.display = 'none';
+        };
+
+    }
+
+    if (Math.abs(currentMenuWidth) <= 0) {
+        mobileMenu.style.display = 'none';
+        return false
+    }
+
+    requestAnimationFrame(menuAnimationClose);
 }
+
 
 //* Event handler *//
 closeMenuBtn.addEventListener('click', menuAnimationClose);
+
+//-------------------------------------//
+
+//************************************************************************************* */
